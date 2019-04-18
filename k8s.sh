64e5ff7ee574c4829963ca1d2960543767de0939
lijiang <450416064@qq.com>
2019-04-18 10:18:45 +0800

    init

---
+++ k8s.sh
@@ -0,0 +1,8 @@
+#!/bin/sh 
+ systemctl restart etcd
+ systemctl restart docker
+ systemctl restart kube-apiserver
+ systemctl restart kube-controller-manager
+ systemctl restart kube-scheduler
+ systemctl restart kubelet
+ systemctl restart kube-proxy
